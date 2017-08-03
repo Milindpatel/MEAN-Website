d02a263f65861cc3324dc248590dbed4cf86237d
Milindpatel <milindpatel3959@gmail.com>
2017-08-03 03:01:57 -0400

    add a router in index.js file for id and added one function to handle that ID for hotel search in hotels.controllers.js file

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -5,4 +5,13 @@ module.exports.hotelsGetAll = function(req,res){
         res
             .status(200)
             .json(hotelData);
-}
+};
+
+module.exports.hotelsGetOne = function(req,res){
+    var hotelId = req.params.hotelId;
+    var thisHotel = hotelData[hotelId];
+    console.log("GET hotelId", hotelId);
+        res
+            .status(200)
+            .json(thisHotel);
+};
