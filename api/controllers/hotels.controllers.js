c7e468bf6d01f8fe1b4f0d64d2ee6b34078cc108
Milindpatel <milindpatel3959@gmail.com>
2017-08-03 22:24:45 -0400

    add new functionality for add new hotel using post method and make changes as per requirement in app.js, index.js and hotels.controllers.js files

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -2,9 +2,22 @@ var hotelData = require('../data/hotel-data.json');
 
 module.exports.hotelsGetAll = function(req,res){
     console.log("GET the hotels");
+    console.log(req.query);
+
+    var offset = 0;
+    var count = 5;
+
+    if(req.query && req.query.offset){
+        offset = parseInt(req.query.offset, 10);
+    }
+
+    if(req.query && req.query.count){
+        count = parseInt(req.query.count, 10);
+    }
+    var returnData = hotelData.slice(offset,offset+count);//get the data from particular json file in slice (particular portion) 
         res
             .status(200)
-            .json(hotelData);
+            .json(returnData);
 };
 
 module.exports.hotelsGetOne = function(req,res){
@@ -14,4 +27,12 @@ module.exports.hotelsGetOne = function(req,res){
         res
             .status(200)
             .json(thisHotel);
+};
+
+module.exports.hotelsAddOne = function(req, res) {
+  console.log("POST new hotel");
+  console.log(req.body);
+  res
+    .status(200)
+    .json(req.body);
 };
