52e8957eb56e38e6ba748c7a6804abdbeacd94da
Milindpatel <milindpatel3959@gmail.com>
2017-09-09 00:34:37 -0400

    add new functionality which one add reviews and display one particular hotel information

--- public/angular-app/hotel-list/hotel-list-controller.js
+++ public/angular-app/hotel-list/hotel-list-controller.js
@@ -1,10 +1,10 @@
-angular.module('meanhotel').controller('HotelsController',HotelsController);
+angular.module('meanhotel').controller('HotelsController', HotelsController);
 
-function HotelsController($http){
-    var vm = this;
-    vm.title = "MEAN Hotel App";
-    $http.get('/api/hotels?count=10').then(function(response){
-        // console.log(response);
-        vm.hotels = response.data;
-    });
-} 
+function HotelsController(hotelDataFactory) {
+  var vm = this;
+  vm.title = 'MEAN Hotel App';
+  hotelDataFactory.hotelList().then(function(response){
+    
+    vm.hotels = response.data;
+  });
+}
