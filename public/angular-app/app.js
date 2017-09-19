26490c3be35043cf0b8b43fe38b5a7aa65a5f70c
Milindpatel <milindpatel3959@gmail.com>
2017-09-19 14:00:19 -0400

    authentication

--- public/angular-app/app.js
+++ public/angular-app/app.js
@@ -5,10 +5,12 @@ function config($routeProvider) {
   $routeProvider
 
   .when('/', {
-     templateUrl: 'angular-app/hotel-list/hotels.html',
-     controller: HotelsController,
-      controllerAs: 'vm',
-      
+     templateUrl : 'angular-app/main/main.html'
+  })
+  .when('/hotels',{
+    templateUrl : 'angular-app/hotel-list/hotel.html',
+    controller : HotelsController,
+    controllerAs : 'vm'
   })
    .when('/hotel/:id',{
      templateUrl : 'angular-app/hotel-display/hotel.html',
@@ -20,6 +22,9 @@ function config($routeProvider) {
      templateUrl : 'angular-app/register/register.html',
      controller : RegisterController,
      controllerAs : 'vm'
-   });
+   })
+   .otherwise({
+     redirectTo :'/'
+   })
 }
 
